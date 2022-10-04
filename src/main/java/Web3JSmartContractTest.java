26ad73b048199a9089e088123b9514a9b7543724
Markus <markus.migenda@union-investment.de>
2022-10-04 15:28:30 +0200

    Some tests

--- src/main/java/Web3JSmartContractTest.java
+++ src/main/java/Web3JSmartContractTest.java
@@ -9,8 +9,8 @@ public class Web3JSmartContractTest {
 
 
   public static void main(String[] args) throws Exception {
-    var client = Web3j.build(new HttpService("HTTP://127.0.0.1:7545\n"));
-    var creds = Credentials.create("c8837169cfb0e1a6d8c517ae8d3df82658a4f273c67505b64a08fba1576bb6e8");  // private key
+    var client = Web3j.build(new HttpService("HTTP://127.0.0.1:8545\n"));
+    var creds = Credentials.create("8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63");  // private key
 
     var gasProvider = new StaticGasProvider(BigInteger.valueOf(1000), BigInteger.valueOf(1000000));
     var greeter = Greeter.deploy(client, creds, gasProvider).send();
